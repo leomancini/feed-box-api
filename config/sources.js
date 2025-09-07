c1566b4a09f20210be679c7815328a2416e5886f
Leo Mancini <mancini@leo.gd>
2025-09-07 09:35:06 -0400

    Removing NASA news as a source

--- config/sources.js
+++ config/sources.js
@@ -2,7 +2,6 @@ import { fetchNYTHeadlines } from "../sources/headlines.js";
 import { sampleStrings } from "../sources/samples.js";
 import { fetchSportsScoreboard } from "../sources/sports.js";
 import { fetchWikipediaContent } from "../sources/wikipedia.js";
-import { fetchNASANews } from "../sources/nasa.js";
 
 export const sourceHandlers = {
   sample: async () => sampleStrings,
@@ -15,5 +14,4 @@ export const sourceHandlers = {
     const type = (req.query.type || "today-featured-article").toLowerCase();
     return await fetchWikipediaContent(type, req);
   },
-  nasa: async (req) => await fetchNASANews(req)
 };
